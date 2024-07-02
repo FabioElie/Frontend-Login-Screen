de807fb3c066b7b0b44e737f1849926b89846a6b
Fabio Elie <fabioelie@outlook.com>
2024-07-02 16:44:02 -0400

    Deixando o codigo mais legivel

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function validateCEP(cep) {
 }
 
 function validateLoginForm() {
-    var email = $('#email-login').val();
-    var password = $('#password-login').val();
+    var email = $("#email-login").val();
+    var password = $("#password-login").val();
 
     if (!validateEmail(email)) {
         alert("Por favor, insira um e-mail válido.");
@@ -25,14 +25,16 @@ function validateLoginForm() {
 }
 
 function validateSignupForm() {
-    var fullName = $('#fullName').val();
-    var cep = $('#cep').val();
-    var email = $('#email').val();
-    var password = $('#password').val();
-    var passwordRepeat = $('#psw-repeat').val();
+    var fullName = $("#fullName").val();
+    var cep = $("#cep").val();
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var passwordRepeat = $("#psw-repeat").val();
 
     if (!validateCEP(cep)) {
-        alert("Por favor, insira um CEP válido no formato XXXXX-XXX ou XXXXXXXX.");
+        alert(
+            "Por favor, insira um CEP válido no formato XXXXX-XXX ou XXXXXXXX."
+        );
         return false;
     }
 
@@ -46,47 +48,53 @@ function validateSignupForm() {
         return false;
     }
 
-    if (fullName === "" || cep === "" || email === "" || password === "" || passwordRepeat === "") {
+    if (
+        fullName === "" ||
+        cep === "" ||
+        email === "" ||
+        password === "" ||
+        passwordRepeat === ""
+    ) {
         alert("Por favor, preencha todos os campos para cadastro.");
         return false;
-    }
+    } 
 
     return true;
 }
 
-$('#loginBtn').click(function (e) {
+$("#loginBtn").click(function (e) {
     if (validateLoginForm()) {
-        alert("Login efetuado com sucesso!")
-        $('#loginForm')[0].reset();
+        alert("Login efetuado com sucesso!");
+        $("#loginForm")[0].reset();
     } else {
         e.preventDefault();
-    }
+                        }
 });
 
-$('#submitBtn').click(function (e) {
+$("#submitBtn").click(function (e) {
     if (validateSignupForm()) {
-        alert("Cadastrado com sucesso!")
-        $('#signupForm')[0].reset();
+        alert("Cadastrado com sucesso!");
+        $("#signupForm")[0].reset();
     } else {
         e.preventDefault();
     }
 });
 
-$('#cancelBtn').click(function () {
-    $('#signupForm')[0].reset();
+$("#cancelBtn").click(function () {
+    $("#signupForm")[0].reset();
 });
 
-$('#showSignupBtn').on('click', function () {
-    $('#signupForm').addClass('active');
-    $('#loginForm').removeClass('active');
-    $('#showSignupBtn').addClass('active');
-    $('#showLoginBtn').removeClass('active');
+$("#showSignupBtn").on("click", function () {
+    $("#signupForm").addClass("active");
+    $("#loginForm").removeClass("active");
+    $("#showSignupBtn").addClass("active");
+    $("#showLoginBtn").removeClass("active");
 });
 
-$('#showLoginBtn').on('click', function () {
-    $('#loginForm').addClass('active');
-    $('#signupForm').removeClass('active');
-    $('#showLoginBtn').addClass('active');
-    $('#showSignupBtn').removeClass('active');
+$("#showLoginBtn").on("click", function () {
+    $("#loginForm").addClass("active");
+    $("#signupForm").removeClass("active");
+    $("#showLoginBtn").addClass("active");
+    $("#showSignupBtn").removeClass("active");
 });
 
